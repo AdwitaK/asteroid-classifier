e515c2ae121ac3276b1a56d3428055de34aa9efe
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-08-19 18:29:55 -0400

    create classifier model

---
+++ src/model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class AsteroidClassifier(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(num_features, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+        )
+
+    def forward(self, x):
+        return self.network(x)
