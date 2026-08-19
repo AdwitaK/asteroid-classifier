e1bd56177959016f2237baf09d1180e6dd044458
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-08-19 15:02:56 -0400

    add conversion to tensors

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,9 @@
+import torch
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-def load():
+def load(data_path):
     # Idenitify features and target
     FEATURES = ["Absolute Magnitude", "Est Dia in KM(min)", "Est Dia in KM(max)", 
                 "Relative Velocity km per sec", "Miss Dist.(kilometers)", "Orbit Uncertainity", 
@@ -14,7 +15,7 @@ def load():
     TARGET = "Hazardous"
 
     # Load into dataframes
-    df = pd.read_csv("../data/nasa.csv")
+    df = pd.read_csv(data_path)
 
     x = df[FEATURES]
     y = df[TARGET].astype(int)
@@ -36,11 +37,33 @@ def split_scale(x, y):
 
     return x_train_scaled, y_train.values, x_val_scaled, y_val.values, x_test_scaled, y_test.values 
 
-def preprocess():
+def preprocess(data_path):
     '''Loads data from data/nasa.csv. Splits data into training (70%), validation (15%), and test (15%) sets.
-    Returns list of numpy arrays in the following order:
+    
+    Parameter: 
+    - data_path: path of dataset
+    
+    Returns: list of numpy arrays in the following order:
     x_train, y_train, x_val, y_val, x_test, y_test'''
 
-    x, y = load()
+    x, y = load(data_path)
     x_train, y_train, x_val, y_val, x_test, y_test = split_scale(x, y)
     return [x_train, y_train, x_val, y_val, x_test, y_test]
+
+def convert_to_tensors(data):
+    '''Optional method to use after preprocess(). Converts list of numpy arrays to torch tensors.
+
+    Parameter: 
+    - data: list of numpy arrays produces by the preprocess method
+    
+    Returns: tensors in the following order   
+        x_train, y_train, x_val, y_val, x_test, y_test'''
+    
+    x_train = torch.tensor(data[0], dtype = torch.float32)
+    y_train = torch.tensor(data[1], dtype = torch.float32)
+    x_val = torch.tensor(data[2], dtype = torch.float32)
+    y_val = torch.tensor(data[3], dtype = torch.float32)
+    x_test = torch.tensor(data[4], dtype = torch.float32)
+    y_test = torch.tensor(data[5], dtype = torch.float32)
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
