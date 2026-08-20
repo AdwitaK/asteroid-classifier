8816478647af8f938630943875c333f0d9b032ea
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-08-20 17:26:04 -0400

    more neuron architecture

--- src/model.py
+++ src/model.py
@@ -4,7 +4,9 @@ class AsteroidClassifier(nn.Module):
     def __init__(self, num_features):
         super().__init__()
         self.network = nn.Sequential(
-            nn.Linear(num_features, 64),
+            nn.Linear(num_features, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
             nn.ReLU(),
             nn.Linear(64, 32),
             nn.ReLU(),
